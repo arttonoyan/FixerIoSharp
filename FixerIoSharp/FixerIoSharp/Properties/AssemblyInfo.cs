458994fae91987d5d3c33a16ab70f6b70b5830a1
Artyom Tonoyan <arttonoyan@gmail.com>
2017-08-11 18:23:13 +0400

    Create The Project.

---
+++ FixerIoSharp/FixerIoSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FixerIoSharp")]
+[assembly: AssemblyDescription("A simple wrapper for the fixer.io currency API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ArtNix LLC")]
+[assembly: AssemblyProduct("FixerIoSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("918bb8bb-b104-4093-aa49-c814776841cd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
